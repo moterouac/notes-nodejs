81f5a275d54ff13052c4fe6b625ddbe25ceff42e
moterro <maute020@gmail.com>
2023-05-29 17:23:00 -0500

    validate user exists when create

--- services/user-service.js
+++ services/user-service.js
@@ -1,8 +1,11 @@
 const { create,getByUidOrAuthId } = require('../repositories/user-repository');
 
+const { ErrorHandler } = require('../config/helpers/error-helper');
 
 const createUser = async (data) => {
     const uid = '';
+    const userExist = await getByUidOrAuthId(uid);
+    if (userExist) throw new ErrorHandler(409, 'User already exists');
     await create(data);
     return getByUidOrAuthId(uid);
   };
